d3ff061ec0c5dfbd1cf19324e51cf20e3ddf0c35
Tharaka Umayanga <tharakau@gmail.com>
2023-06-04 19:49:17 +0530

    Updated __init__.py

---
+++ vedastro/unittests/__init__.py
@@ -0,0 +1,17 @@
+# import os
+from pythonnet import load
+
+load("coreclr")
+import clr
+
+# loading project dlls
+current_path = os.path.dirname(os.path.abspath(__file__))
+
+dll_path = os.path.join(current_path, 'dlls', 'VedAstro.Library.dll')
+full_path = os.path.abspath(dll_path)
+clr.AddReference(full_path)
+#
+# from ..planets import Planets
+# from ..astronomical_calculator import AstronomicalCalculator
+# from ..time import Time
+# from ..geolocation import GeoLocation
