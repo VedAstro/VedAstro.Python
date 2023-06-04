e90b99d51ac4004efce8dee2d87067b8edfc8d0f
Tharaka Umayanga <tharakau@gmail.com>
2023-06-04 11:55:40 +0530

    accesing dll files relative to package

--- vedastro/__init__.py
+++ vedastro/__init__.py
@@ -4,6 +4,12 @@ load("coreclr")
 import clr
 
 # loading project dlls
-dll_path = os.path.join('dlls', 'VedAstro.Library.dll')
+current_path = os.path.dirname(os.path.abspath(__file__))
+
+dll_path = os.path.join(current_path,'dlls', 'VedAstro.Library.dll')
 full_path = os.path.abspath(dll_path)
 clr.AddReference(full_path)
+
+# importing calc functions
+from .calc import *
+
