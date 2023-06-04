9363b0f3cf3a513a85d269abcff6fc16b59cb8fb
Tharaka Umayanga <tharakau@gmail.com>
2023-06-04 13:18:02 +0530

    added dataclass Planets and imported in __init__.py

---
+++ vedastro/planets.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+
+from VedAstro.Library import PlanetName
+
+
+@dataclass
+class Planets:
+    """
+    This class represents the nine planets in Vedic astrology.
+
+    Attributes:
+        sun (PlanetName): The sun planet.
+        moon (PlanetName): The moon planet.
+        mars (PlanetName): The mars planet.
+        mercury (PlanetName): The mercury planet.
+        jupiter (PlanetName): The jupiter planet.
+        venus (PlanetName): The venus planet.
+        saturn (PlanetName): The saturn planet.
+        ketu (PlanetName): The ketu planet.
+        rahu (PlanetName): The rahu planet.
+    """
+    sun = PlanetName.Sun
+    moon = PlanetName.Moon
+    mars = PlanetName.Mars
+    mercury = PlanetName.Mercury
+    jupiter = PlanetName.Jupiter
+    venus = PlanetName.Venus
+    saturn = PlanetName.Saturn
+    ketu = PlanetName.Ketu
+    rahu = PlanetName.Rahu
