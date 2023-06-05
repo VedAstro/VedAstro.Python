7f4be3fcacf0bbeb230ea6c9dbc04fa77adf2d87
Tharaka Umayanga <tharakau@gmail.com>
2023-06-05 11:01:32 +0530

    added eventcalculator.py

---
+++ vedastro/calculators/eventcalculator.py
@@ -0,0 +1,85 @@
+import VedAstro.Library as libray
+
+
+def Empty(time: Time, person: Person):
+    return libray.Empty(time, person)
+
+
+def IsGoodTarabalaOccuring(time: Time, person: Person):
+    return libray.IsGoodTarabalaOccuring(time, person)
+
+
+def IsBadTarabalaOccuring(time: Time, person: Person):
+    return libray.IsBadTarabalaOccuring(time, person)
+
+
+def IsTarabalaJanmaStrongOccuring(time: Time, person: Person):
+    return libray.IsTarabalaJanmaStrongOccuring(time, person)
+
+
+def IsTarabalaSampatStrongOccuring(time: Time, person: Person):
+    return libray.IsTarabalaSampatStrongOccuring(time, person)
+
+
+def IsTarabalaVipatStrongOccuring(time: Time, person: Person):
+    return libray.IsTarabalaVipatStrongOccuring(time, person)
+
+
+def IsTarabalaKshemaStrongOccuring(time: Time, person: Person):
+    return libray.IsTarabalaKshemaStrongOccuring(time, person)
+
+
+def IsTarabalaPratyakStrongOccuring(time: Time, person: Person):
+    return libray.IsTarabalaPratyakStrongOccuring(time, person)
+
+
+def IsTarabalaSadhanaStrongOccuring(time: Time, person: Person):
+    return libray.IsTarabalaSadhanaStrongOccuring(time, person)
+
+
+def IsTarabalaNaidhanaStrongOccuring(time: Time, person: Person):
+    return libray.IsTarabalaNaidhanaStrongOccuring(time, person)
+
+
+def IsTarabalaMitraStrongOccuring(time: Time, person: Person):
+    return libray.IsTarabalaMitraStrongOccuring(time, person)
+
+
+def IsTarabalaParamaMitraStrongOccuring(time: Time, person: Person):
+    return libray.IsTarabalaParamaMitraStrongOccuring(time, person)
+
+
+def IsTarabalaJanmaMiddlingOccuring(time: Time, person: Person):
+    return libray.IsTarabalaJanmaMiddlingOccuring(time, person)
+
+
+def IsTarabalaSampatMiddlingOccuring(time: Time, person: Person):
+    return libray.IsTarabalaSampatMiddlingOccuring(time, person)
+
+
+def IsTarabalaVipatMiddlingOccuring(time: Time, person: Person):
+    return libray.IsTarabalaVipatMiddlingOccuring(time, person)
+
+
+def IsTarabalaKshemaMiddlingOccuring(time: Time, person: Person):
+    return libray.IsTarabalaKshemaMiddlingOccuring(time, person)
+
+
+def IsTarabalaPratyakMiddlingOccuring(time: Time, person: Person):
+    return libray.IsTarabalaPratyakMiddlingOccuring(time, person)
+
+
+def IsTarabalaSadhanaMiddlingOccuring(time: Time, person: Person):
+    return libray.IsTarabalaSadhanaMiddlingOccuring(time, person)
+
+
+def IsTarabalaNaidhanaMiddlingOccuring(time: Time, person: Person):
+    return libray.IsTarabalaNaidhanaMiddlingOccuring(time, person)
+
+
+def IsTarabalaMitraMiddlingOccuring(time: Time, person: Person):
+    return libray.IsTarabalaMitraMiddlingOccuring(time, person)
+
+
+def IsTarabalaParamaMitraMiddlingOccuring(time: Time, person: Person):
+    return libray.IsTarabalaParamaMitraMiddlingOccuring(time, person)
