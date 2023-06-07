e4b7fc45593ab559e9f3f6bbc131cafb03053655
Tharaka Umayanga <tharakau@gmail.com>
2023-06-07 07:56:55 +0530

    updated matchcalculator.py.py

--- vedastro/calculators/matchcalculator.py
+++ vedastro/calculators/matchcalculator.py
@@ -6,68 +6,68 @@ from vedastro.objects.matchreport import MatchReport
 
 
 def GetNewMatchReport(male: Person, female: Person, user_id: str) -> MatchReport:
-    return libray.GetNewMatchReport(male, female, user_id)
+    return libray.MatchCalculator.GetNewMatchReport(male, female, user_id)
 
 
 def Mahendra(male: Person, female: Person) -> MatchPrediction:
-    return libray.Mahendra(male, female)
+    return libray.MatchCalculator.Mahendra(male, female)
 
 
 def NadiKuta(male: Person, female: Person) -> MatchPrediction:
-    return libray.NadiKuta(male, female)
+    return libray.MatchCalculator.NadiKuta(male, female)
 
 
 def GunaKuta(male: Person, female: Person) -> MatchPrediction:
-    return libray.GunaKuta(male, female)
+    return libray.MatchCalculator.GunaKuta(male, female)
 
 
 def Varna(male: Person, female: Person) -> MatchPrediction:
-    return libray.Varna(male, female)
+    return libray.MatchCalculator.Varna(male, female)
 
 
 def YoniKuta(male: Person, female: Person) -> MatchPrediction:
-    return libray.YoniKuta(male, female)
+    return libray.MatchCalculator.YoniKuta(male, female)
 
 
 def VedhaKuta(male: Person, female: Person) -> MatchPrediction:
-    return libray.VedhaKuta(male, female)
+    return libray.MatchCalculator.VedhaKuta(male, female)
 
 
 def Rajju(male: Person, female: Person) -> MatchPrediction:
-    return libray.Rajju(male, female)
+    return libray.MatchCalculator.Rajju(male, female)
 
 
 def VasyaKuta(male: Person, female: Person) -> MatchPrediction:
-    return libray.VasyaKuta(male, female)
+    return libray.MatchCalculator.VasyaKuta(male, female)
 
 
 def GrahaMaitram(male: Person, female: Person) -> MatchPrediction:
-    return libray.GrahaMaitram(male, female)
+    return libray.MatchCalculator.GrahaMaitram(male, female)
 
 
 def RasiKuta(male: Person, female: Person) -> MatchPrediction:
-    return libray.RasiKuta(male, female)
+    return libray.MatchCalculator.RasiKuta(male, female)
 
 
 def StreeDeergha(male: Person, female: Person) -> MatchPrediction:
-    return libray.StreeDeergha(male, female)
+    return libray.MatchCalculator.StreeDeergha(male, female)
 
 
 def DinaKuta(male: Person, female: Person) -> MatchPrediction:
-    return libray.DinaKuta(male, female)
+    return libray.MatchCalculator.DinaKuta(male, female)
 
 
 def LagnaAndHouse7Good(male: Person, female: Person) -> MatchPrediction:
-    return libray.LagnaAndHouse7Good(male, female)
+    return libray.MatchCalculator.LagnaAndHouse7Good(male, female)
 
 
 def KujaDosa(male: Person, female: Person) -> MatchPrediction:
-    return libray.KujaDosa(male, female)
+    return libray.MatchCalculator.KujaDosa(male, female)
 
 
 def BadConstellations(male: Person, female: Person) -> MatchPrediction:
-    return libray.BadConstellations(male, female)
+    return libray.MatchCalculator.BadConstellations(male, female)
 
 
 def SexEnergyCompatibility(male: Person, female: Person) -> MatchPrediction:
-    return libray.SexEnergyCompatibility(male, female)
+    return libray.MatchCalculator.SexEnergyCompatibility(male, female)
