db17e70914759ba6fd88781129f2995ab3325958
Tharaka Umayanga <tharakau@gmail.com>
2023-06-09 13:12:46 +0530

    added package data

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='vedastro',
+    version='0.1',
+    packages=['vedastro', 'vedastro.objects', 'vedastro.unittests', 'vedastro.calculators'],
+    url='https://github.com/VedAstro/VedAstro.Python',
+    license='MIT',
+    author='',
+    author_email='',
+    description='A Python wrapper libray for VedAstro '
+)
