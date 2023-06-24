a0021d81b66fe89014d3dcf6727a394b75592ec9
Tharaka Umayanga <tharakau@gmail.com>
2023-06-24 12:49:23 +0530

    added zodiacname and zodiacsign

---
+++ vedastro/objects/zodiacname.py
@@ -0,0 +1,16 @@
+import VedAstro.Library as libray
+
+
+class ZodiacName:
+    Aries = libray.ZodiacName.Aries,
+    Taurus = libray.ZodiacName.Taurus,
+    Gemini = libray.ZodiacName.Gemini,
+    Cancer = libray.ZodiacName.Cancer,
+    Leo = libray.ZodiacName.Leo,
+    Virgo = libray.ZodiacName.Virgo,
+    Libra = libray.ZodiacName.Libra,
+    Scorpio = libray.ZodiacName.Scorpio,
+    Sagittarius = libray.ZodiacName.Sagittarius,
+    Capricornus = libray.ZodiacName.Capricornus,
+    Aquarius = libray.ZodiacName.Aquarius,
+    Pisces = libray.ZodiacName.Pisces
