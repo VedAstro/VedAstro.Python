2ffb477dece1456331cf4efaf2776c4fc69feaef
Tharaka Umayanga <tharakau@gmail.com>
2023-06-24 13:33:03 +0530

    added eventmanager docstrings

--- vedastro/calculators/eventmanager.py
+++ vedastro/calculators/eventmanager.py
@@ -10,6 +10,16 @@ class EventManager:
     @staticmethod
     def calculate_events(events_precision: float, start_time: Time, end_time: Time,
                          geo_location: GeoLocation, person: Person, inputed_event_tags: List[EventTag]):
+        """
+        Calculate the events between the start time and end time with the given precision.
+        :param events_precision:
+        :param start_time:
+        :param end_time:
+        :param geo_location:
+        :param person:
+        :param inputed_event_tags:
+        :return:
+        """
         event_list = libray.EventManager.CalculateEvents(events_precision, start_time, end_time, geo_location, person,
                                                          inputed_event_tags)
         return event_list
