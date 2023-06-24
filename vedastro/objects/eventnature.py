74da6f2525d7a6a2a763b5a01c80408ef603551d
Tharaka Umayanga <tharakau@gmail.com>
2023-06-24 12:47:51 +0530

    added EventNature

---
+++ vedastro/objects/eventnature.py
@@ -0,0 +1,8 @@
+import VedAstro.Library as library
+
+
+class EventNature:
+    Empty = library.EventNature.Empty,
+    Good = library.EventNature.Good,
+    Neutral = library.EventNature.Neutral,
+    Bad = library.EventNature.Bad
