18f7b70911c8f1bae58c2a93c27ca89e2b7ee07b
Tharaka Umayanga <tharakau@gmail.com>
2023-06-24 12:49:05 +0530

    added PlanetName , Prediction , shashtimas and time_preset

---
+++ vedastro/objects/time_preset.py
@@ -0,0 +1,13 @@
+import VedAstro.Library as library
+
+
+class TimePreset:
+    Minute1 = library.TimePreset.Minute1
+    Minute2 = library.TimePreset.Minute2
+    Minute3 = library.TimePreset.Minute3
+    Minute4 = library.TimePreset.Minute4
+    Minute5 = library.TimePreset.Minute5
+    Minute10 = library.TimePreset.Minute10
+    Minute15 = library.TimePreset.Minute15
+    Minute30 = library.TimePreset.Minute30
+    Hour1 = library.TimePreset.Hour1
