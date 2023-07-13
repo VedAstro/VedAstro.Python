8a88c1aadd17fcc8a0272ec7bd8cc2993364a358
Tharaka Umayanga <tharakau@gmail.com>
2023-07-13 10:22:50 +0530

    added runtimeconfig.json

--- vedastro/__init__.py
+++ vedastro/__init__.py
@@ -1,13 +1,15 @@
 import os
 from pythonnet import load
-load("coreclr")
+
+current_path = os.path.dirname(os.path.abspath(__file__))
+
+runtimeconfig_path = os.path.abspath(os.path.join(current_path, 'runtimeconfig.json'))
+print(runtimeconfig_path)
+load("coreclr", runtime_config=runtimeconfig_path)
 import clr
 
 # loading project dlls
-current_path = os.path.dirname(os.path.abspath(__file__))
 
-dll_path = os.path.join(current_path,'dlls', 'VedAstro.Library.dll')
+dll_path = os.path.join(current_path, 'dlls', 'VedAstro.Library.dll')
 full_path = os.path.abspath(dll_path)
 clr.AddReference(full_path)
-
-
