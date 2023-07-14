550ca6f9057c614b8c37191017bdf968035993b0
Tharaka Umayanga <tharakau@gmail.com>
2023-07-14 15:52:07 +0530

    re arranged code

--- vedastro/__init__.py
+++ vedastro/__init__.py
@@ -2,14 +2,11 @@ import os
 from pythonnet import load
 
 current_path = os.path.dirname(os.path.abspath(__file__))
-
-runtimeconfig_path = os.path.abspath(os.path.join(current_path, 'runtimeconfig.json'))
-print(runtimeconfig_path)
-load("coreclr", runtime_config=runtimeconfig_path)
-import clr
-
-# loading project dlls
-
 dll_path = os.path.join(current_path, 'dlls', 'VedAstro.Library.dll')
 full_path = os.path.abspath(dll_path)
+
+runtime_config_path = os.path.abspath(os.path.join(current_path, 'runtimeconfig.json'))
+
+load("coreclr", runtime_config=runtime_config_path)
+import clr
 clr.AddReference(full_path)
