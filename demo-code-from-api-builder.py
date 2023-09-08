d797d7511fff39a0ed4462bc84ae7164a04a0353
Vignes <vignes@live.com.my>
2023-09-08 22:27:51 +0800

    working copy paste dynamic code sample

--- demo-code-from-api-builder.py
+++ demo-code-from-api-builder.py
@@ -1,8 +1,17 @@
 import vedastro # install via pip
 from VedAstro.Library import * # reference full library
 
-
+# set location
 geolocation = GeoLocation("Tokyo, Japan", 139.83, 35.65)
 
+# set time hh:mm dd/mm/yyyy zzz
+time = Time("23:40 31/12/2010 +08:00", geolocation)
 
-print(geolocation)
+# set planet
+planetName = PlanetName.Sun
+
+# run calculator to get result
+calcResult = Calculate.HousePlanetIsIn(time, planetName)
+
+# display results
+Tools.Print(calcResult)
