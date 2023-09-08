dcdd45bdf8a185817e0fed128849540c3c4c15f9
Vignes <vignes@live.com.my>
2023-09-08 21:12:12 +0800

    improve comments, make "clr" available in global

--- vedastro/__init__.py
+++ vedastro/__init__.py
@@ -1,24 +1,23 @@
-import os
+import os # used to get file paths
 from pythonnet import load
 
 
-def _initialize_VedAstro():
-    """
-    Initialize the VedAstro by loading the necessary DLL and setting up the runtime configuration.
-    """
-    current_path = os.path.dirname(os.path.abspath(__file__))
-    dll_path = os.path.join(current_path, 'dlls', 'VedAstro.Library.dll')
-    full_path = os.path.abspath(dll_path)
+ # initialize the VedAstro by loading the necessary DLL and setting up the runtime configuration.
+current_path = os.path.dirname(os.path.abspath(__file__))
+dll_path = os.path.join(current_path, 'dlls', 'VedAstro.Library.dll')
+full_path = os.path.abspath(dll_path)
 
-    runtime_config_path = os.path.abspath(os.path.join(current_path, 'runtimeconfig.json'))
-    try:
+# load CLR settings first, else won't work
+runtime_config_path = os.path.abspath(os.path.join(current_path, 'runtimeconfig.json'))
+load("coreclr", runtime_config=runtime_config_path)
+    
+try:
+    #note CLR can only be imported AFTER runtime config has been set above
+    import clr # provided by the Pythonnet package
+    clr.AddReference(full_path)
 
-        load("coreclr", runtime_config=runtime_config_path)
-        import clr
-        clr.AddReference(full_path)
-    except RuntimeError:
-        print(f"Dotnet 7 not found. please visit https://dotnet.microsoft.com/en-us/download/dotnet/7.0")
+except RuntimeError:
+    print(f"Dotnet 7 not found. please visit https://dotnet.microsoft.com/en-us/download/dotnet/7.0")
 
 
-# Call the function to initialize the VedAstro
-_initialize_VedAstro()
+        
