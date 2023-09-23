08bc1fdee16edaa3237ec2ec51a66679ea161e1c
Sengiv <vignes@live.com.my>
2023-09-23 10:58:45 +0800

    update to fix dependency issues

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="VedAstro",
-    version="1.0.0",
-    license='MIT',
-    description="An open source Python library for advanced astronomical calculations like planet longitude, house sign, shadbala, etc...",
-    author="VedAstro",
-    author_email="developer@vedastro.org",
-    packages=find_packages(include=["vedastro"]),
+    name='VedAstro',
+    version='1.0.0',
+    description='An open source Python library for advanced astronomical calculations like planet longitude, house sign, shadbala, etc...',
+    author='Tharaka Umayanga',
+    author_email='tharakau@gmail.com',
+    packages=find_packages(include=['vedastro']),
     install_requires=[
-        "packaging",
-        "pythonnet",
-        "pycparser"
+        'packaging',
+        'pythonnet==3.0.2',
+        'pycparser',
     ],
-    python_requires=">=3.9.*",
+    python_requires='>=3.9,<3.12',
 )
