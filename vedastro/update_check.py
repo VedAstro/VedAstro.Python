a6c29284e05ae8190b06397c2a83433cf2235723
Sengiv <vignes@live.com.my>
2023-09-23 20:05:14 +0800

    online version check bug fix

--- vedastro/update_check.py
+++ vedastro/update_check.py
@@ -1,21 +1,22 @@
 import pkg_resources
 from packaging import version
+import requests
 import subprocess
 
 
-# checks pip package if outdated
 def check_for_update(package_name):
-    
     # Get the installed version
     installed_version = pkg_resources.get_distribution(package_name).version
     
     # Get the latest version
-    pypi_info = subprocess.check_output(f"pip show {package_name}", shell=True).decode()
-    latest_version = [line.split(": ")[1] for line in pypi_info.split("\n") if "Version" in line][0]
+    response = requests.get(f'https://pypi.org/pypi/{package_name}/json')
+    latest_version = response.json()['info']['version']
     
     # Compare versions
     if version.parse(installed_version) < version.parse(latest_version):
-        print(f"A new version of {package_name} is available. You are currently using version {installed_version}, but version {latest_version} is available.")
+        print(f"A better version of the awesome VedAstro package is available. Your version is {installed_version}, latest is {latest_version}.")
         update = input("Would you like to update now? (y/n) ")
         if update.lower() == 'y':
             subprocess.check_call(f"pip install --upgrade {package_name}", shell=True)
+    # else:
+    #     print(f"You are using the latest version of {package_name}. Installed version and latest version are {installed_version}.")
