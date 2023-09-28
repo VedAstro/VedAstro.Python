4988ee9f24abddb5effc35f2986f25d9d20fd96f
Vignes <vignes@live.com.my>
2023-09-28 15:41:25 +0800

    improved

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='VedAstro',
-    version='1.1.7',
+    version='1.3.0',
     description='An open source Python library for advanced astronomical calculations like planet longitude, house sign, shadbala, etc...',
     author='Tharaka Umayanga',
     author_email='tharakau@gmail.com',
     packages=find_packages(include=['vedastro']),
     install_requires=[
         'packaging',
+        'colorama',
+        'requests',
         'pythonnet==3.0.2',
         'pycparser',
     ],
