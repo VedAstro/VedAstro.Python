2731a06eb4ccc2b93cf5f949b435b0b3ef9f7a92
Vignes <vignes@live.com.my>
2023-09-28 16:00:55 +0800

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='VedAstro',
-    version='1.4.0',
+    version='1.5.0',
     description='An open source Python library for advanced astronomical calculations like planet longitude, house sign, shadbala, etc...',
     author='Tharaka Umayanga',
     author_email='tharakau@gmail.com',
