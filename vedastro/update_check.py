e748beb456d42801521dc78e71c1b09192d72e6a
Vignes <vignes@live.com.my>
2023-09-28 15:41:25 +0800

    improved

--- vedastro/update_check.py
+++ vedastro/update_check.py
@@ -1,3 +1,4 @@
+from colorama import Fore, Style
 import pkg_resources
 from packaging import version
 import requests
@@ -6,17 +7,19 @@ import subprocess
 
 def check_for_update(package_name):
     try:
+        
         # Get the installed version
         installed_version = pkg_resources.get_distribution(package_name).version
+        
         # Get the latest version
         response = requests.get(f'https://pypi.org/pypi/{package_name}/json')
         latest_version = response.json()['info']['version']
+        
         # Compare versions
         if version.parse(installed_version) < version.parse(latest_version):
-            print(f"A better version of the awesome VedAstro package is available. Your version is {installed_version}, latest is {latest_version}.")
-            update = input("Would you like to update now? (y/n) ")
-            if update.lower() == 'y':
-                subprocess.check_call(f"pip install --upgrade {package_name}", shell=True)
+            print(Fore.YELLOW + f"VedAstro Package Outdated! Your version is {installed_version}, latest is {latest_version}." + Style.RESET_ALL)
+            subprocess.check_call(f"pip install --upgrade {package_name}", shell=True)
+    
     except Exception as e:
         # print(f"An error occurred: {e}")
         return
