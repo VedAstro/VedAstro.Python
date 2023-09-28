b24060e15c544e02f7fb1e76f6b19b9a913128f5
Vignes <vignes@live.com.my>
2023-09-28 15:59:53 +0800

    improved update messaging

--- vedastro/update_check.py
+++ vedastro/update_check.py
@@ -15,9 +15,13 @@ def check_for_update(package_name):
         response = requests.get(f'https://pypi.org/pypi/{package_name}/json')
         latest_version = response.json()['info']['version']
         
-        # Compare versions
+        # Compare versions & start AUTO Update if possible
+        # NOTE: default to auto update for easy server use
         if version.parse(installed_version) < version.parse(latest_version):
-            print(Fore.YELLOW + f"VedAstro Package Outdated! Your version is {installed_version}, latest is {latest_version}." + Style.RESET_ALL)
+            #warn user
+            print(Fore.YELLOW + f"VedAstro Package Outdated! {installed_version} --> {latest_version}" + Style.RESET_ALL)
+            #start auto update
+            print(Fore.GREEN + f"Starting Update..." + Style.RESET_ALL)
             subprocess.check_call(f"pip install --upgrade {package_name}", shell=True)
     
     except Exception as e:
