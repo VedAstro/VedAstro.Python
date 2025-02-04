d1b2501f5064c0f5cd603c9afe3cb5bc6ea1c1f8
Sengiv <vignes@live.com.my>
2025-02-05 01:49:12 +0800

    minor

--- vedastro/__init__.py
+++ vedastro/__init__.py
@@ -4,8 +4,8 @@ import os # used to get file paths
 print("VedAstro - Made & Funded by Users - https://vedastro.org/Donate\n")
 
 # check if current package is up to date else warn user
-#from .update_check import check_for_update
-#check_for_update("vedastro")
+from .update_check import check_for_update
+check_for_update("vedastro")
 
 from.vedastro import *
 
