0b9eb5fd750587fee5d38c87e0966e27c41006b6
Sengiv <vignes@live.com.my>
2025-02-05 01:47:19 +0800

    match checker demo

---
+++ demo_match_checker.py
@@ -0,0 +1,21 @@
+from vedastro import *  # install via pip
+
+# PART 0 : Set API key
+Calculate.SetAPIKey('xxxxxxxxxxxxxxxxxx')  # replace with your actual API key
+
+#PART 1 : PREPARE NEEDED DATA
+#-----------------------------------
+
+# set birth location
+geolocation = GeoLocation("Tokyo, Japan", 139.83, 35.65)
+
+romeo_birth_time = Time("23:40 31/12/1996 +08:00", geolocation)
+
+juliet_birth_time = Time("23:40 31/12/1997 +08:00", geolocation)
+
+
+#PART 2 : CALCULATE
+#-----------------------------------
+matchReport = Calculate.MatchReport(romeo_birth_time, juliet_birth_time)
+
+print(json.dumps(matchReport, indent=4))
