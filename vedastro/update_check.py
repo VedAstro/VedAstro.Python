c42163268030f9cd60e09f235836f835884511ae
Sengiv <vignes@live.com.my>
2025-02-05 14:22:21 +0800

    Auto stash before merge of "main" and "origin/main"

--- vedastro/update_check.py
+++ vedastro/update_check.py
@@ -19,9 +19,9 @@ def check_for_update(package_name):
         # NOTE: default to auto update for easy server use
         if version.parse(installed_version) < version.parse(latest_version):
             #warn user
-            print(Fore.YELLOW + f"VedAstro Package Outdated! {installed_version} --> {latest_version}" + Style.RESET_ALL)
+            print(Fore.YELLOW + f"{installed_version} --> {latest_version}" + Style.RESET_ALL)
             #start auto update
-            print(Fore.GREEN + f"Starting Update..." + Style.RESET_ALL)
+            print(Fore.GREEN + f"" + Style.RESET_ALL)
             subprocess.check_call(f"pip install --upgrade {package_name}", shell=True)
     
     except Exception as e:
